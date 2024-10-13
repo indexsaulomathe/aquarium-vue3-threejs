e75fe1c075fec58eee6a3e8853d673e9340a3fde
Saulo Matheus de Rezende <saulo_matheprog@hotmail.com>
2024-10-13 01:30:43 -0300

    base config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/<aquarium-vue3-threejs>/' : '/',
+  base: '/aquarium-vue3-threejs/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
